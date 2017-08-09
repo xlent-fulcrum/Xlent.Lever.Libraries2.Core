689eff0b72fba7fa581cc8dca65cf36d313bed5c
magnuswiderberg <magnus.widerberg@xlent.se>
2017-08-09 14:36:50 +0200

    0.5.0 Removed Tenant from IFulcrumLogger

--- src/Core.NetStandard/Logging/Model/IFulcrumLogger.cs
+++ src/Core.NetStandard/Logging/Model/IFulcrumLogger.cs
@@ -1,5 +1,4 @@
 ﻿using System.Threading.Tasks;
-using Xlent.Lever.Libraries2.Core.MultiTenant.Model;
 
 namespace Xlent.Lever.Libraries2.Core.Logging.Model
 {
@@ -9,15 +8,8 @@ namespace Xlent.Lever.Libraries2.Core.Logging.Model
     public interface IFulcrumLogger
     {
         /// <summary>
-        /// Log <paramref name="message"/> for <paramref name="tenant"/> with level <paramref name="logSeverityLevel"/>.
+        /// Log <paramref name="message"/> with level <paramref name="logSeverityLevel"/>.
         /// </summary>
-        /// <remarks>Same as <see cref="Log"/>, but asynchronous.</remarks>
-        Task LogAsync(ITenant tenant, LogSeverityLevel logSeverityLevel, string message);
-
-        /// <summary>
-        /// Log <paramref name="message"/> for <paramref name="tenant"/> with level <paramref name="logSeverityLevel"/>.
-        /// </summary>
-        /// <remarks>Same as <see cref="LogAsync"/>, but synchronous.</remarks>
-        void Log(ITenant tenant, LogSeverityLevel logSeverityLevel, string message);
+        Task LogAsync(LogSeverityLevel logSeverityLevel, string message);
     }
 }
