50fa6a2373040942bed029a2039c17502fea1a01
magnuswiderberg <magnus.widerberg@xlent.se>
2017-08-25 13:34:52 +0200

    0.10.5 Convenience for choosing the right IValueProvider

--- src/Core.NetStandard/Context/ContextValueProvider.cs
+++ src/Core.NetStandard/Context/ContextValueProvider.cs
@@ -1,20 +1,36 @@
-﻿namespace Xlent.Lever.Libraries2.Core.Context
+﻿using Xlent.Lever.Libraries2.Core.Assert;
+
+namespace Xlent.Lever.Libraries2.Core.Context
 {
     /// <summary>
     /// Convenience for choosing the right <see cref="IValueProvider"/>.
     /// </summary>
     public class ContextValueProvider
     {
-        static ContextValueProvider()
-        {
-            Chosen = RecommendedForNetCore;
-        }
+        /// <summary>
+        /// The chosen <see cref="IValueProvider"/> to use.
+        /// </summary>
+        /// <remarks>There are overrides for this, see e.g. in Xlent.Lever.Libraries2.WebApi.Context.</remarks>
+        protected static IValueProvider _chosen;
 
         /// <summary>
         /// The chosen <see cref="IValueProvider"/> to use.
         /// </summary>
         /// <remarks>There are overrides for this, see e.g. in Xlent.Lever.Libraries2.WebApi.Context.</remarks>
-        public static IValueProvider Chosen { get; set; }
+        public static IValueProvider ContextForApplication
+        {
+            get
+            {
+                // TODO: Link to Lever WIKI
+                FulcrumAssert.IsNotNull(_chosen, null, "The application must at startup set this to the appropriate IValueProvider.");
+                return _chosen;
+            }
+            set
+            {
+                InternalContract.RequireNotNull(value, nameof(value));
+                _chosen = value;
+            }
+        }
 
         /// <summary>
         /// Default <see cref="IValueProvider"/> for .NET Core.
