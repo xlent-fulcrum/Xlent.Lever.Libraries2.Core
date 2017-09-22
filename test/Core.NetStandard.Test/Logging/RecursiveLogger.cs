348311972235a952e3e3169c3e2985ef421847e1
Lars Lindberg <lars.lindberg@xlent.se>
2017-09-22 10:03:38 +0200

    Added a test for logging.

---
+++ test/Core.NetStandard.Test/Logging/RecursiveLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xlent.Lever.Libraries2.Core.Application;
+
+namespace Xlent.Lever.Libraries2.Core.Logging
+{
+    internal class RecursiveLogger : IFulcrumFullLogger
+    {
+        public static string Message { get; private set; }
+        public static bool HasFailed { get; private set; }
+        public static bool IsRunning { get; set; }
+        private static readonly object ClassLock = new object();
+
+        /// <inheritdoc />
+        public void Log(LogSeverityLevel logSeverityLevel, string message)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <inheritdoc />
+        public async Task LogAsync(LogInstanceInformation message)
+        {
+            var uniqueString = Guid.NewGuid().ToString();
+            var recursive = message.Message == uniqueString;
+            if (recursive || !Logging.Log.OnlyForUnitTest_LoggingInProgress)
+            {
+                if (!HasFailed)
+                {
+                    HasFailed = true;
+                    Message =
+                        $"The {nameof(LogAsync)}() method should never be called recursively. {nameof(recursive)} = {recursive}, {nameof(Logging.Log.OnlyForUnitTest_LoggingInProgress)} = {Logging.Log.OnlyForUnitTest_LoggingInProgress}";
+                }
+            }
+            // Try to provoke a recursive log call of this method
+            Logging.Log.LogError(uniqueString);
+            IsRunning = false;
+            await Task.Yield();
+        }
+    }
+}
---
+++ test/Core.NetStandard.Test/Logging/TestLogging.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xlent.Lever.Libraries2.Core.Application;
+using Xlent.Lever.Libraries2.Core.Context;
+using UT = Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Xlent.Lever.Libraries2.Core.Logging
+{
+    [TestClass]
+    public class TestLogging
+    {
+        [TestInitialize]
+        public void Initialize()
+        {
+            FulcrumApplicationHelper.UnitTestSetup(typeof(TestLogging).FullName);
+        }
+
+        [TestMethod]
+        public void DetectRecursiveLogging()
+        {
+            FulcrumApplication.Setup.FullLogger = new RecursiveLogger();
+            RecursiveLogger.IsRunning = true;
+            Logging.Log.LogInformation("Top level logging 1, will be followed by recursive logging that should be detected");
+            while (RecursiveLogger.IsRunning) Thread.Sleep(TimeSpan.FromMilliseconds(100));
+            Thread.Sleep(TimeSpan.FromSeconds(1.0));
+            UT.Assert.IsFalse(Logging.Log.OnlyForUnitTest_LoggingInProgress, "Should never happen between logs");
+            Logging.Log.LogInformation("Top level logging 2, will be followed by recursive logging that should be detected");
+            UT.Assert.IsFalse(RecursiveLogger.HasFailed, RecursiveLogger.Message);
+        }
+    }
+}
