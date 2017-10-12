ea9118755f2e3948a5341203c7a623723e18b8d3
magnuswiderberg <magnus.widerberg@xlent.se>
2017-10-12 15:57:07 +0200

    Added class BusinessEventMetaData

---
+++ src/Core.NetStandard/Platform/BusinessEvents/BusinessEventMetaData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Xlent.Lever.Libraries2.Core.Platform.BusinessEvents
+{
+    /// <summary>
+    /// Meta data present at publication of all Business Events
+    /// </summary>
+    public class BusinessEventMetaData
+    {
+        /// <summary>The technical name of the publishing client</summary>
+        public string PublisherName { get; set; }
+
+        /// <summary>The name of the entity</summary>
+        public string EntityName { get; set; }
+
+        /// <summary>The name of the event, in the context of the <see cref="EntityName"/></summary>
+        public string EventName { get; set; }
+
+        /// <summary>The major version of the event</summary>
+        public int MajorVersion { get; set; }
+
+        /// <summary>The minor version of the event</summary>
+        public int MainorVersion { get; set; }
+
+        /// <summary>The timestamp for when the event was published</summary>
+        public DateTimeOffset PublishedAt { get; set; }
+    }
+}
