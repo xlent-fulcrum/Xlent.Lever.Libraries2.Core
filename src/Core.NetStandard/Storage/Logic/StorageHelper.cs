c01530c2f7fce1a8563629c428d9096e851d6993
Lars Lindberg <lars.lindberg@xlent.se>
2017-12-03 10:04:23 +0100

    Added StorageHelper.cs

---
+++ src/Core.NetStandard/Storage/Logic/StorageHelper.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xlent.Lever.Libraries2.Core.Assert;
+
+namespace Xlent.Lever.Libraries2.Core.Storage.Logic
+{
+    public static class StorageHelper
+    {
+        public static TId CreateNewId<TId>()
+        {
+            var id = default(TId);
+            if (typeof(TId) == typeof(Guid))
+            {
+                // ReSharper disable once SuspiciousTypeConversion.Global
+                id = (dynamic)Guid.NewGuid();
+            }
+            else if (typeof(TId) == typeof(string))
+            {
+                // ReSharper disable once SuspiciousTypeConversion.Global
+                id = (dynamic)Guid.NewGuid().ToString();
+            }
+            else
+            {
+                FulcrumAssert.Fail(null,
+                    $"{nameof(CreateNewId)} can handle Guid and string as type for Id, but it can't handle {typeof(TId)}.");
+            }
+            return id;
+        }
+    }
+}
