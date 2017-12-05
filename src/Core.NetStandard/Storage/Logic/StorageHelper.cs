b4e48641233ee2d962e361cd53095ea8d72df5a2
Lars Lindberg <lars.lindberg@xlent.se>
2017-12-05 23:20:12 +0100

    Merge problems fixed.

--- src/Core.NetStandard/Storage/Logic/StorageHelper.cs
+++ src/Core.NetStandard/Storage/Logic/StorageHelper.cs
@@ -1,10 +1,19 @@
 ﻿using System;
+using Newtonsoft.Json;
 using Xlent.Lever.Libraries2.Core.Assert;
 
 namespace Xlent.Lever.Libraries2.Core.Storage.Logic
 {
-    internal static class StorageHelper
+    /// <summary>
+    /// Helper methods for Storage
+    /// </summary>
+    public static class StorageHelper
     {
+        /// <summary>
+        /// Create a new Id of type <see cref="string"/> or type <see cref="Guid"/>.
+        /// </summary>
+        /// <typeparam name="TId"></typeparam>
+        /// <returns></returns>
         public static TId CreateNewId<TId>()
         {
             var id = default(TId);
@@ -24,6 +33,16 @@ namespace Xlent.Lever.Libraries2.Core.Storage.Logic
                     $"{nameof(CreateNewId)} can handle Guid and string as type for Id, but it can't handle {typeof(TId)}.");
             }
             return id;
+        }
+
+        /// <summary>
+        /// A generic method for deep copying.
+        /// </summary>
+        /// <param name="source">The object that should be copied.</param>
+        /// <returns>A copied object.</returns>
+        public static T DeepCopy<T>(T source)
+        {
+            return JsonConvert.DeserializeObject<T>(JsonConvert.SerializeObject(source));
         }
     }
 }
