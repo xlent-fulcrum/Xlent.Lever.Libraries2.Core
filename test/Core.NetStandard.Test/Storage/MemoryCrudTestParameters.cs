3c6507c7613ee5498ab9e8a90cfe0e529e7b1afa
Lars Lindberg <lars.lindberg@xlent.se>
2018-04-20 10:14:45 +0200

    Tests pass for Memory.

---
+++ test/Core.NetStandard.Test/Storage/MemoryCrudTestParameters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Xlent.Lever.Libraries2.Core.Application;
+using Xlent.Lever.Libraries2.Core.Crud.Interfaces;
+using Xlent.Lever.Libraries2.Core.Crud.MemoryStorage;
+using Xlent.Lever.Libraries2.Core.Storage.Logic;
+using Xlent.Lever.Libraries2.Core.Storage.Model;
+using Xlent.Lever.Libraries2.Core.Test.NuGet;
+using Xlent.Lever.Libraries2.Core.Test.NuGet.Crud;
+using Xlent.Lever.Libraries2.Core.Test.NuGet.Model;
+
+namespace Xlent.Lever.Libraries2.Core.Storage
+{
+    [TestClass]
+    public class MemoryCrudTestParameters : TestParameters
+    {
+
+        /// <inheritdoc />
+        public MemoryCrudTestParameters() : base(new CrudMemory<TestItemBare, Guid>())
+        {
+            FulcrumApplicationHelper.UnitTestSetup(nameof(MemoryCrudTestParameters));
+        }
+    }
+}
