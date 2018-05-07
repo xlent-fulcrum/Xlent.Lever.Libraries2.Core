eb6f805cdc1f0307066090fd7e5018ff5f28d64a
Lars Lindberg <lars.lindberg@xlent.se>
2018-05-07 09:51:27 +0200

    Removed obsolete AsyncLazy.cs. Added more documentation for StackTraceHiddenAttribute.

--- src/Core.NetStandard/Misc/StackTraceHiddenAttribute.cs
+++ src/Core.NetStandard/Misc/StackTraceHiddenAttribute.cs
@@ -5,6 +5,8 @@ namespace Xlent.Lever.Libraries2.Core.Misc
 {
     /// <summary>
     /// Attribute to hide methods from <see cref="FulcrumException"/> stack traces.
+    /// Add this to methods that you don't want to be part of the stack trace.
+    /// Typically used for FulcrumAssert methods.
     /// </summary>
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
     public sealed class StackTraceHiddenAttribute : Attribute
