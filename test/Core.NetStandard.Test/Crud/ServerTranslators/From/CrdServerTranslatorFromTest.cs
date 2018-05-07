395cdc0f8a9306f9d21caa10afa6da431c9b1256
Lars Lindberg <lars.lindberg@xlent.se>
2018-05-07 22:22:42 +0200

    100% coverage of ServerTranslators/From.

--- test/Core.NetStandard.Test/Crud/ServerTranslators/From/CrdServerTranslatorFromTest.cs
+++ test/Core.NetStandard.Test/Crud/ServerTranslators/From/CrdServerTranslatorFromTest.cs
@@ -1,67 +1,156 @@
-﻿using System.Threading;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 using Xlent.Lever.Libraries2.Core.Crud.Interfaces;
 using Xlent.Lever.Libraries2.Core.Crud.ServerTranslators.From;
 using Xlent.Lever.Libraries2.Core.NetFramework.Test.Core.Crud.ServerTranslators.Support;
+using Xlent.Lever.Libraries2.Core.Storage.Model;
 using Xlent.Lever.Libraries2.Core.Translation;
 
 namespace Xlent.Lever.Libraries2.Core.NetFramework.Test.Core.Crud.ServerTranslators.From
 {
     [TestClass]
-    public class CrdServerTranslatorFromTest
+    public class CrdServerTranslatorFromTest : ServerTranslatorBase
     {
-        private Mock<ICrd<TestModelCreate, TestModel, string>> _storageMock;
         private ICrd<TestModelCreate, TestModel, string> _serviceToTest;
-        private const string ClientName = "client-name";
-        private const string ServerName = "server-name";
-        private const string TestModelServerId = "server-1";
-        private const string TestModelClientId = "client-1";
-        private const string TestModelClientStatus = "client-status-a";
-        private const string TestModelServerStatus = "server-status-a";
 
         [TestInitialize]
         public void Initialize()
         {
-            _storageMock = new Mock<ICrd<TestModelCreate, TestModel, string>>();
-            var serverTranslator = new CrdServerTranslatorFrom<TestModelCreate, TestModel>(_storageMock.Object, TestModel.IdConceptName,
+            StorageMock = new Mock<ICrd<TestModelCreate, TestModel, string>>();
+            var serverTranslator = new CrdServerTranslatorFrom<TestModelCreate, TestModel>(StorageMock.Object, TestModel.IdConceptName,
                 () => ServerName);
             _serviceToTest = serverTranslator;
         }
 
         [TestMethod]
-        public async Task CreateAsync()
+        public async Task CreateAsync_Sunshine()
         {
-            _storageMock
+            StorageMock
                 .Setup(crd =>
                     crd.CreateAsync(It.Is<TestModelCreate>(create => create.Status == TestModelServerStatus), CancellationToken.None))
                 .ReturnsAsync(TestModelServerId);
-            var item = GetServerTestModel();
-            var id = await _serviceToTest.CreateAsync(item);
+            var serverInItem = GetServerTestModel();
+            var id = await _serviceToTest.CreateAsync(serverInItem);
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(id);
             Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(TestModel.DecoratedId(ServerName, TestModelServerId), id);
-            _storageMock.Verify();
+            StorageMock.Verify();
         }
 
-        private static TestModel GetClientTestModel()
+        [TestMethod]
+        public async Task CreateAndReturnAsync_Sunshine()
         {
-            var item = new TestModel()
-            {
-                Id = TestModelClientId,
-                Status = TestModelClientStatus
-            };
-            return item;
+            var expectedItem = Decorate(ServerName, GetServerTestModel());
+            var serverInItem = GetServerTestModel();
+            var serverOutItem = GetServerTestModel();
+            StorageMock
+                .Setup(crd =>
+                    crd.CreateAndReturnAsync(It.Is<TestModelCreate>(create => create.Status == TestModelServerStatus), CancellationToken.None))
+                .ReturnsAsync(serverOutItem);
+            var decoratedItem = await _serviceToTest.CreateAndReturnAsync(serverInItem);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(decoratedItem);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expectedItem, decoratedItem);
+            StorageMock.Verify();
         }
 
-        private static TestModel GetServerTestModel()
+        [TestMethod]
+        public async Task CreateWithSpecifiedIdAsync_Sunshine()
         {
-            var item = new TestModel()
-            {
-                Id = TestModelServerId,
-                Status = TestModelServerStatus
-            };
-            return item;
+            StorageMock
+                .Setup(crd =>
+                    crd.CreateWithSpecifiedIdAsync(It.Is<string>(id => id == TestModelServerId),
+                        It.Is<TestModelCreate>(create => create.Status == TestModelServerStatus),
+                        CancellationToken.None)).Returns(Task.CompletedTask);
+            var serverInItem = GetServerTestModel();
+            await _serviceToTest.CreateWithSpecifiedIdAsync(TestModelServerId, serverInItem);
+            StorageMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task CreateWithSpecifiedIdAndReturnAsync_Sunshine()
+        {
+            var expectedItem = Decorate(ServerName, GetServerTestModel());
+            var serverInItem = GetServerTestModel();
+            var serverOutItem = GetServerTestModel();
+            StorageMock
+                .Setup(crd =>
+                    crd.CreateWithSpecifiedIdAndReturnAsync(It.Is<string>(id => id == TestModelServerId), It.Is<TestModelCreate>(create => create.Status == TestModelServerStatus), CancellationToken.None))
+                .ReturnsAsync(serverOutItem);
+            var decoratedItem = await _serviceToTest.CreateWithSpecifiedIdAndReturnAsync(TestModelServerId, serverInItem);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(decoratedItem);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expectedItem, decoratedItem);
+            StorageMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task ReadAsync_Sunshine()
+        {
+            var expectedItem = Decorate(ServerName, GetServerTestModel());
+            var serverOutItem = GetServerTestModel();
+            StorageMock
+                .Setup(crd =>
+                    crd.ReadAsync(It.Is<string>(id => id == TestModelServerId),
+                        CancellationToken.None)).ReturnsAsync(serverOutItem);
+            var decoratedItem = await _serviceToTest.ReadAsync(TestModelServerId);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(decoratedItem);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(expectedItem, decoratedItem);
+            StorageMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task ReadAllAsync_Sunshine()
+        {
+            var expectedItems = new[] { Decorate(ServerName, GetServerTestModel()) };
+
+            var serverOutItems = new[] { GetServerTestModel() };
+            StorageMock
+                .Setup(crd =>
+                    crd.ReadAllAsync(It.IsAny<int>(), CancellationToken.None)).ReturnsAsync(serverOutItems);
+            var decoratedItems = await _serviceToTest.ReadAllAsync();
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(decoratedItems);
+            AreEqual(expectedItems, decoratedItems);
+            StorageMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task ReadAllWithPagingAsync_Sunshine()
+        {
+            var expectedItems = new[] { Decorate(ServerName, GetServerTestModel()) };
+            var expectedPage = new PageEnvelope<TestModel>(0, 50, 1, expectedItems);
+
+            var serverOutItems = new[] { GetServerTestModel() };
+            var pageOut = new PageEnvelope<TestModel>(expectedPage.PageInfo, serverOutItems);
+            StorageMock
+                .Setup(crd =>
+                    crd.ReadAllWithPagingAsync(0, It.IsAny<int?>(), CancellationToken.None)).ReturnsAsync(pageOut);
+            var decoratedPage = await _serviceToTest.ReadAllWithPagingAsync(0);
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsNotNull(decoratedPage?.Data);
+            AreEqual(expectedPage, decoratedPage);
+            StorageMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task DeleteAsync_Sunshine()
+        {
+            StorageMock
+                .Setup(crd =>
+                    crd.DeleteAsync(It.Is<string>(id => id == TestModelServerId),
+                        CancellationToken.None)).Returns(Task.CompletedTask);
+            await _serviceToTest.DeleteAsync(TestModelServerId);
+            StorageMock.Verify();
+        }
+
+        [TestMethod]
+        public async Task DeleteAllAsync_Sunshine()
+        {
+            StorageMock
+                .Setup(crd =>
+                    crd.DeleteAllAsync(CancellationToken.None)).Returns(Task.CompletedTask);
+            await _serviceToTest.DeleteAllAsync();
+            StorageMock.Verify();
         }
     }
 }
--- test/Core.NetStandard.Test/Crud/ServerTranslators/Support/TestModel.cs
+++ test/Core.NetStandard.Test/Crud/ServerTranslators/Support/TestModel.cs
@@ -1,8 +1,10 @@
 ﻿using Xlent.Lever.Libraries2.Core.Storage.Model;
 using Xlent.Lever.Libraries2.Core.Translation;
+#pragma warning disable 659
 
 namespace Xlent.Lever.Libraries2.Core.NetFramework.Test.Core.Crud.ServerTranslators.Support
 {
+    /// <inheritdoc cref="TestModelCreate" />
     public class TestModel : TestModelCreate, IUniquelyIdentifiable<string>, IOptimisticConcurrencyControlByETag
     {
         public const string IdConceptName = "testmodel.id";
@@ -17,5 +19,13 @@ namespace Xlent.Lever.Libraries2.Core.NetFramework.Test.Core.Crud.ServerTranslat
         public static string DecoratedId(string name, string id) => $"({IdConceptName}!~{name}!{id})";
 
         public string DecoratedId(string name) => DecoratedId(name, Id);
+
+        /// <inheritdoc />
+        public override bool Equals(object obj)
+        {
+            if (obj == null) return false;
+            if (!(obj is TestModel testModel)) return false;
+            return Equals(Id, testModel.Id) && base.Equals(obj);
+        }
     }
 }
