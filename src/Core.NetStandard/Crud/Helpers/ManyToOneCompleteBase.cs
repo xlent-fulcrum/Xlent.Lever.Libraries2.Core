7f54a5c91bae6b981443489d7c3c204f36dd7c8a
Lars Lindberg <lars.lindberg@xlent.se>
2018-05-12 10:17:13 +0200

    Added missing overload class.

--- src/Core.NetStandard/Crud/Helpers/ManyToOneCompleteBase.cs
+++ src/Core.NetStandard/Crud/Helpers/ManyToOneCompleteBase.cs
@@ -7,17 +7,23 @@ using Xlent.Lever.Libraries2.Core.Storage.Model;
 
 namespace Xlent.Lever.Libraries2.Core.Crud.Helpers
 {
+    /// <inheritdoc cref="ManyToOneCompleteBase{TModelCreate,TModel,TId}" />
+    public abstract class ManyToOneCompleteBase< TModel, TId> : ManyToOneCompleteBase<TModel, TModel, TId>,
+        IManyToOneComplete<TModel, TId>
+    {
+    }
+
     /// <summary>
     /// Abstract base class that has a default implementation for <see cref="ReadChildrenAsync"/>
     /// and <see cref="DeleteChildrenAsync"/>.
     /// </summary>
-    public abstract class ManyToOneCompleteBase<TModelCreate, TModel, TId> : CrudBase<TModelCreate, TModel, TId>, IManyToOneComplete<TModelCreate, TModel, TId> 
+    public abstract class ManyToOneCompleteBase<TModelCreate, TModel, TId> : CrudBase<TModelCreate, TModel, TId>, IManyToOneComplete<TModelCreate, TModel, TId>
         where TModel : TModelCreate
     {
         /// <inheritdoc />
         public abstract Task<PageEnvelope<TModel>> ReadChildrenWithPagingAsync(TId parentId, int offset,
-            int? limit = null,
-            CancellationToken token = default(CancellationToken));
+        int? limit = null,
+        CancellationToken token = default(CancellationToken));
 
         /// <inheritdoc />
         public virtual Task<IEnumerable<TModel>> ReadChildrenAsync(TId parentId, int limit = Int32.MaxValue, CancellationToken token = default(CancellationToken))
